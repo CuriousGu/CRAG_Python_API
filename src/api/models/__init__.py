a23ed1e9b1e72704752f6bd772e80749e867bb3f
Pedro HB Ribeiro <pedro.hbueno03@usp.br>
2025-02-19 18:07:12 -0300

    [feat] add document management in inits

--- src/api/models/__init__.py
+++ src/api/models/__init__.py
@@ -1,3 +1,4 @@
 from .api import APIResponse, APIRequest
+from .documents_request import DocumentRequest  
 
-__all__ = ["APIResponse", "APIRequest"]
+__all__ = ["APIResponse", "APIRequest", "DocumentRequest"]
